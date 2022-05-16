9d238acec36a81cfaf07aeba0765a0fa13a2de44
lightclient@protonmail.com <lightclient@protonmail.com>
2022-05-15 20:22:51 -0600

    update prysm to dev branch, add domain logic to signing

---
+++ types/signing.go
@@ -0,0 +1,91 @@
+package types
+
+import (
+	"encoding/binary"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/prysmaticlabs/prysm/crypto/bls"
+)
+
+type Domain [32]byte
+type DomainType uint32
+
+var (
+	DomainBuilder         Domain
+	GenesisValidatorsRoot Root
+)
+
+const (
+	DomainTypeBeaconProposer DomainType = 0x00000000
+	DomainTypeAppBuilder     DomainType = 0x00000001
+)
+
+func init() {
+	DomainBuilder = ComputeApplicationDomain(DomainTypeAppBuilder)
+	GenesisValidatorsRoot = [32]byte(common.HexToHash("0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"))
+}
+
+type SigningData struct {
+	Root   Root   `ssz-size:"32"`
+	Domain Domain `ssz-size:"32"`
+}
+
+type forkData struct {
+	CurrentVersion        uint32
+	GenesisValidatorsRoot Root `ssz-size:"32"`
+}
+
+type HashTreeRoot interface {
+	HashTreeRoot() ([32]byte, error)
+}
+
+func ComputeDomain(dt DomainType, forkVersion uint32, genesisValidatorsRoot *Root) [32]byte {
+	if genesisValidatorsRoot == nil {
+		var tmp Root
+		genesisValidatorsRoot = &tmp
+	}
+	forkDataRoot, _ := (&forkData{
+		CurrentVersion:        forkVersion,
+		GenesisValidatorsRoot: *genesisValidatorsRoot,
+	}).HashTreeRoot()
+
+	var domain [32]byte
+	binary.LittleEndian.PutUint32(domain[0:4], uint32(dt))
+	copy(domain[4:], forkDataRoot[0:28])
+
+	return domain
+}
+
+func ComputeApplicationDomain(dt DomainType) [32]byte {
+	return ComputeDomain(dt, 0, nil)
+}
+
+func ComputeSigningRoot(obj HashTreeRoot, d Domain) ([32]byte, error) {
+	var zero [32]byte
+	root, err := obj.HashTreeRoot()
+	if err != nil {
+		return zero, err
+	}
+	signingData := SigningData{root, d}
+	msg, err := signingData.HashTreeRoot()
+	if err != nil {
+		return zero, err
+	}
+	return msg, nil
+}
+
+func VerifySignature(obj HashTreeRoot, d Domain, pk, s []byte) (bool, error) {
+	msg, err := ComputeSigningRoot(obj, d)
+	if err != nil {
+		return false, err
+	}
+	sig, err := bls.SignatureFromBytes(s)
+	if err != nil {
+		return false, err
+	}
+	pubkey, err := bls.PublicKeyFromBytes(pk)
+	if err != nil {
+		return false, err
+	}
+	return sig.Verify(pubkey, msg[:]), nil
+}
---
+++ types/signing_test.go
@@ -0,0 +1,34 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/prysmaticlabs/prysm/crypto/bls"
+	"github.com/stretchr/testify/require"
+)
+
+func newKeypair(t *testing.T) (pubkey []byte, privkey bls.SecretKey) {
+	sk, err := bls.RandKey()
+	if err != nil {
+		t.Fatal("unable to generate bls key pair", err)
+	}
+	return sk.PublicKey().Marshal(), sk
+}
+
+func TestVerifySignature(t *testing.T) {
+	pk, sk := newKeypair(t)
+	msg := &RegisterValidatorRequestMessage{
+		FeeRecipient: Address{0x42},
+		GasLimit:     15_000_000,
+		Timestamp:    1652369368,
+		Pubkey:       PublicKey{0x0d},
+	}
+	domain := ComputeApplicationDomain(0x01)
+	root, err := ComputeSigningRoot(msg, domain)
+	require.NoError(t, err)
+
+	sig := sk.Sign(root[:])
+	ok, err := VerifySignature(msg, domain, pk, sig.Marshal())
+	require.NoError(t, err)
+	require.True(t, ok)
+}
