d5e9a4088f2dd6e742e9ad187c2b68c7ef3a48f6
Justin Traglia <jtraglia@pm.me>
2022-07-09 10:37:47 -0500

    Clean up test function

--- types/builder_test.go
+++ types/builder_test.go
@@ -205,27 +205,25 @@ func TestMerkelizePayload(t *testing.T) {
 }
 
 func TestIndexedAttestation(t *testing.T) {
-    input := `
-    {
-      "attesting_indices": [
-        "1", "2", "3"
-      ],
-      "signature": "0x1c66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505",
-      "data": {
-        "slot": "1",
-        "index": "1",
-        "beacon_block_root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
-        "source": {
-          "epoch": "1",
-          "root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"
-        },
-        "target": {
-          "epoch": "1",
-          "root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"
-        }
-      }
-    }
-    `
+	input := `{
+		"attesting_indices": [
+			"1", "2", "3"
+		],
+		"signature": "0x1c66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505",
+		"data": {
+			"slot": "1",
+			"index": "1",
+			"beacon_block_root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+			"source": {
+				"epoch": "1",
+				"root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"
+			},
+			"target": {
+				"epoch": "1",
+				"root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"
+			}
+		}
+	}`
 	var result IndexedAttestation
 	err := json.Unmarshal([]byte(input), &result)
 	require.NoError(t, err)
