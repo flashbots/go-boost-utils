672417f81fe37e3fdd3d2c6e0ca484862a718e61
terencechain <terence@prysmaticlabs.com>
2022-07-29 09:43:08 -0700

    Update Prater/Goerli versions and genesis root (#32)

--- types/common.go
+++ types/common.go
@@ -15,19 +15,19 @@ var (
 	GenesisValidatorsRootKiln    = "0x99b09fcd43e5905236c370f184056bec6e6638cfc31a323b304fc4aa789cb4ad"
 	GenesisValidatorsRootRopsten = "0x44f1e56283ca88b35c789f7f449e52339bc1fefe3a45913a43a6d16edcd33cf1"
 	GenesisValidatorsRootSepolia = "0xd8ea171f3c94aea21ebc42a1ed61052acf3f9209c00e4efbaaddac09ed9b8078"
-	// GenesisValidatorsRootGoerli  = ""
+	GenesisValidatorsRootGoerli  = "0x043db0d9a83813551ee2f33450d23797757d430911a9320530ad8a0eabc43efb"
 
 	// GenesisForkVersionMainnet = "0x00000000"
 	GenesisForkVersionKiln    = "0x70000069"
 	GenesisForkVersionRopsten = "0x80000069"
 	GenesisForkVersionSepolia = "0x90000069"
-	// GenesisForkVersionGoerli  = ""
+	GenesisForkVersionGoerli  = "0x00001020"
 
 	// BellatrixForkVersionMainner = ""
 	BellatrixForkVersionKiln    = "0x70000071"
 	BellatrixForkVersionRopsten = "0x80000071"
 	BellatrixForkVersionSepolia = "0x90000071"
-	// BellatrixForkVersionGoerli  = ""
+	BellatrixForkVersionGoerli  = "0x02001020"
 
 	ErrLength = fmt.Errorf("incorrect byte length")
 )
