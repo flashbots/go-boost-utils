f7f8dbabb59649621c3ca1d8fb731b0493880879
Chris Hager <chris@linuxuser.at>
2022-09-19 12:20:09 +0200

    added consts (#41)

---
+++ types/consts.go
@@ -0,0 +1,23 @@
+package types
+
+const (
+	DepositContractTreeDepth = 32
+
+	GenesisValidatorsRootKiln    = "0x99b09fcd43e5905236c370f184056bec6e6638cfc31a323b304fc4aa789cb4ad"
+	GenesisValidatorsRootRopsten = "0x44f1e56283ca88b35c789f7f449e52339bc1fefe3a45913a43a6d16edcd33cf1"
+	GenesisValidatorsRootSepolia = "0xd8ea171f3c94aea21ebc42a1ed61052acf3f9209c00e4efbaaddac09ed9b8078"
+	GenesisValidatorsRootGoerli  = "0x043db0d9a83813551ee2f33450d23797757d430911a9320530ad8a0eabc43efb"
+	GenesisValidatorsRootMainnet = "0x4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95"
+
+	GenesisForkVersionKiln    = "0x70000069"
+	GenesisForkVersionRopsten = "0x80000069"
+	GenesisForkVersionSepolia = "0x90000069"
+	GenesisForkVersionGoerli  = "0x00001020"
+	GenesisForkVersionMainnet = "0x00000000"
+
+	BellatrixForkVersionKiln    = "0x70000071"
+	BellatrixForkVersionRopsten = "0x80000071"
+	BellatrixForkVersionSepolia = "0x90000071"
+	BellatrixForkVersionGoerli  = "0x02001020"
+	BellatrixForkVersionMainnet = "0x02000000"
+)
