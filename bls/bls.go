bd78989b06035c1cd92bbbf4ea02175e131761cf
Justin Traglia <95511699+jtraglia@users.noreply.github.com>
2023-03-19 15:12:21 +0000

    Replace kilic with gnark-crypto (#62)
    * Replace kilic with gnark-crypto
    
    * Delete comment & use size vars
    
    * Add some extra checks & organize
    
    * Remove unnecessary types
    
    * Rename two errors
    
    * Move domain to var group
    
    * Use short name for variable
    
    * Remove extra checks & use Jac value with ScalarMults
    
    * Use version of gnark-crypto that was audited

--- bls/bls.go
+++ bls/bls.go
@@ -1,31 +1,28 @@
 package bls
 
 import (
-	"crypto/rand"
 	"errors"
+	"math/big"
 
-	bls12381 "github.com/kilic/bls12-381"
+	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
+	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 )
 
-// Heavily inspired by:
-// https://github.com/protolambda/bls12-381-util/blob/master/signatures.go
-// Thank you for the excellent code.
-
-var domain = []byte("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_")
-
 const (
-	PublicKeyLength int = 48
-	SecretKeyLength int = 32
-	SignatureLength int = 96
+	PublicKeyLength = bls12381.SizeOfG1AffineCompressed
+	SecretKeyLength = fr.Bytes
+	SignatureLength = bls12381.SizeOfG2AffineCompressed
 )
 
 type (
-	PublicKey = bls12381.PointG1
-	SecretKey = bls12381.Fr
-	Signature = bls12381.PointG2
+	PublicKey = bls12381.G1Affine
+	SecretKey = fr.Element
+	Signature = bls12381.G2Affine
 )
 
 var (
+	g1OneJac, _, g1One, _     = bls12381.Generators()
+	domain                    = []byte("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_")
 	ErrInvalidPubkeyLength    = errors.New("invalid public key length")
 	ErrInvalidSecretKeyLength = errors.New("invalid secret key length")
 	ErrInvalidSignatureLength = errors.New("invalid signature length")
@@ -33,47 +30,61 @@ var (
 )
 
 func PublicKeyToBytes(pk *PublicKey) []byte {
-	return bls12381.NewG1().ToCompressed(pk)
+	pkBytes := pk.Bytes()
+	return pkBytes[:]
 }
 
 func SecretKeyToBytes(sk *SecretKey) []byte {
-	return sk.ToBytes()
+	skBytes := sk.Bytes()
+	return skBytes[:]
 }
 
 func SignatureToBytes(sig *Signature) []byte {
-	return bls12381.NewG2().ToCompressed(sig)
+	sigBytes := sig.Bytes()
+	return sigBytes[:]
 }
 
 func PublicKeyFromBytes(pkBytes []byte) (*PublicKey, error) {
 	if len(pkBytes) != PublicKeyLength {
 		return nil, ErrInvalidPubkeyLength
 	}
-	return bls12381.NewG1().FromCompressed(pkBytes)
-}
-
-func PublicKeyFromSecretKey(sk *SecretKey) (*PublicKey, error) {
-	if sk.IsZero() {
-		return nil, ErrSecretKeyIsZero
-	}
 	pk := new(PublicKey)
-	g1 := bls12381.NewG1()
-	g1.MulScalar(pk, &bls12381.G1One, sk)
-	return pk, nil
+	err := pk.Unmarshal(pkBytes)
+	return pk, err
 }
 
 func SecretKeyFromBytes(skBytes []byte) (*SecretKey, error) {
 	if len(skBytes) != SecretKeyLength {
 		return nil, ErrInvalidSecretKeyLength
 	}
-	sk := bls12381.NewFr().FromBytes(skBytes)
+	sk := new(SecretKey).SetBytes(skBytes)
 	if sk.IsZero() {
 		return nil, ErrSecretKeyIsZero
 	}
 	return sk, nil
 }
 
+func SignatureFromBytes(sigBytes []byte) (*Signature, error) {
+	if len(sigBytes) != SignatureLength {
+		return nil, ErrInvalidSignatureLength
+	}
+	sig := new(Signature)
+	err := sig.Unmarshal(sigBytes)
+	return sig, err
+}
+
 func GenerateRandomSecretKey() (*SecretKey, error) {
-	return new(SecretKey).Rand(rand.Reader)
+	return new(SecretKey).SetRandom()
+}
+
+func PublicKeyFromSecretKey(sk *SecretKey) (*PublicKey, error) {
+	if sk.IsZero() {
+		return nil, ErrSecretKeyIsZero
+	}
+	skBigInt := new(big.Int)
+	sk.ToBigIntRegular(skBigInt)
+	pkJac := new(bls12381.G1Jac).ScalarMultiplication(&g1OneJac, skBigInt)
+	return new(bls12381.G1Affine).FromJacobian(pkJac), nil
 }
 
 func GenerateNewKeypair() (*SecretKey, *PublicKey, error) {
@@ -89,34 +100,28 @@ func GenerateNewKeypair() (*SecretKey, *PublicKey, error) {
 }
 
 func Sign(sk *SecretKey, msg []byte) *Signature {
-	g2 := bls12381.NewG2()
-	Q, err := g2.HashToCurve(msg, domain)
+	Q, err := bls12381.HashToG2(msg, domain)
 	if err != nil {
 		panic(err)
 	}
-	var signature bls12381.PointG2
-	g2.MulScalar(&signature, Q, sk)
-	return &signature
-}
-
-func SignatureFromBytes(sigBytes []byte) (*Signature, error) {
-	if len(sigBytes) != SignatureLength {
-		return nil, ErrInvalidSignatureLength
-	}
-	return bls12381.NewG2().FromCompressed(sigBytes)
+	skBigInt := new(big.Int)
+	sk.ToBigIntRegular(skBigInt)
+	QJac := new(bls12381.G2Jac).FromAffine(&Q)
+	sigJac := new(bls12381.G2Jac).ScalarMultiplication(QJac, skBigInt)
+	return new(bls12381.G2Affine).FromJacobian(sigJac)
 }
 
 func VerifySignature(sig *Signature, pk *PublicKey, msg []byte) (bool, error) {
-	Q, err := bls12381.NewG2().HashToCurve(msg, domain)
+	Q, err := bls12381.HashToG2(msg, domain)
 	if err != nil {
 		return false, err
 	}
-	P := &bls12381.G1One
-
-	pairingEngine := bls12381.NewEngine()
-	pairingEngine.AddPair(pk, Q)
-	pairingEngine.AddPairInv(P, sig)
-	return pairingEngine.Check(), nil
+	var negP bls12381.G1Affine
+	negP.Neg(&g1One)
+	return bls12381.PairingCheck(
+		[]bls12381.G1Affine{*pk, negP},
+		[]bls12381.G2Affine{Q, *sig},
+	)
 }
 
 func VerifySignatureBytes(msg, sigBytes, pkBytes []byte) (bool, error) {
@@ -128,6 +133,5 @@ func VerifySignatureBytes(msg, sigBytes, pkBytes []byte) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-
 	return VerifySignature(sig, pk, msg)
 }
